f7524942b85e3b2f8217364c442aab2a45de0e4b
Shivantha56 <dilshanshivantha3@gmail.com>
2026-05-19 23:30:46 +0530

    User createInertia to define react app

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from "@vitejs/plugin-react"
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import inertia from '@inertiajs/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.jsx'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
@@ -17,6 +18,7 @@ export default defineConfig({
         }),
         tailwindcss(),
         react(),
+        inertia()
     ],
     server: {
         watch: {
